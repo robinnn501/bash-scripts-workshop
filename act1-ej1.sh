b53ee5687378c8ee6fe1ff92b983c1f60e090283
ies <rgasteluc@ies-sabadell.cat>
2025-05-13 18:48:36 +0200

    Added act1-ej1.sh

---
+++ act1-ej1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Variables globales:"
+echo "HOME=$HOME"
+echo "USER=$USER"
+echo "SHELL=$SHELL"
+echo "PWD=$PWD"
+echo "PATH=$PATH"
+
+echo "Argumentos: $@"
+echo "Último valor de retorno: $?"
+echo "PID de la shell actual: $$"
